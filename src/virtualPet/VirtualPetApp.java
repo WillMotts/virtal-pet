d417b190e0008402b80b73159246a4737f95492c
WillMotts <willmmmotts@outlook.com>
2017-10-06 09:11:52 -0500

    Virtual Pet

---
+++ src/virtualPet/VirtualPetApp.java
@@ -0,0 +1,16 @@
+package virtualPet;
+
+import java.util.Scanner;
+
+public class VirtualPetApp {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		VirtualPet account = new VirtualPet();
+		
+		System.out.println("Hello! Welcome to Virtual Pet simulator!");
+		System.out.println("\nWhat will you name your pet?");
+		
+	}
+
+}
